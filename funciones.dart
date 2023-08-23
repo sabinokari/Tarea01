d530d005a8b928196e8e82ce4532f1904a67026d
SABINO KARI B <202100977b@utea.edu.pe>
2023-08-23 07:38:29 -0500

    mapas

---
+++ funciones.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  mostrarmensaje();
+  print(suma(5, 3));
+}
+
+mostrarmensaje() {
+  print(" Hola Mundo !!!! desde una funcion");
+  print(" Hola Mundo !!!! desde una funcion");
+  print(" Hola Mundo !!!! desde una funcion");
+  print(" Hola Mundo !!!! desde una funcion");
+}
+
+/*void suma() {
+  print("La suma es:  ${2 + 2}");
+*/
+int suma(int n1, int n2) {
+  print("La sum ess: ");
+  return n1 + n2;
+}
---
+++ multiplicacion.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  mostrarmensaje();
+  print(multiplicacion(5, 3));
+}
+
+mostrarmensaje() {}
+
+int multiplicacion(int n1, int n2) {
+  print("La multiplicacion ess: ");
+  return n1 * n2;
+}
